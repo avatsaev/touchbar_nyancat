302954cd91cec785556239dabe2f143c63f913ff
A. Vatsaev <azero79@gmail.com>
2016-11-05 20:41:33 +0100

    Initial Commit

---
+++ touchbar_nyancat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  touchbar_nyancat
+//
+//  Created by Aslan Vatsaev on 05/11/2016.
+//  Copyright © 2016 AVatsaev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ touchbar_nyancat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  touchbar_nyancat
+//
+//  Created by Aslan Vatsaev on 05/11/2016.
+//  Copyright © 2016 AVatsaev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
